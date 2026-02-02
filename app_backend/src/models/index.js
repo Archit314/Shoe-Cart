2677b2c05b2744c7af54f217a7a6e94e7bf3646c
Archit Kumar <architkumar314@gmail.com>
2026-02-02 18:39:56 +0530

    [Backend]: Fixes for the ES6 importing

--- app_backend/src/models/index.js
+++ app_backend/src/models/index.js
@@ -1,12 +1,17 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
+import fs from 'fs';
+import path from 'path';
+import Sequelize from 'sequelize';
+import process from 'process';
+import { fileURLToPath, pathToFileURL } from 'url';
+import configDefault from '../config/sequelize.config.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/sequelize.config.js')[env];
+const config = configDefault[env];
 const db = {};
 
 let sequelize;
@@ -16,7 +21,7 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs
+const files = fs
   .readdirSync(__dirname)
   .filter(file => {
     return (
@@ -25,11 +30,19 @@ fs
       file.slice(-3) === '.js' &&
       file.indexOf('.test.js') === -1
     );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
   });
+
+for (const file of files) {
+  const filePath = path.join(__dirname, file);
+  const imported = await import(pathToFileURL(filePath).href);
+  const modelFactory = imported.default || imported;
+  const model = typeof modelFactory === 'function'
+    ? modelFactory(sequelize, Sequelize.DataTypes)
+    : modelFactory;
+  if (model && model.name) {
+    db[model.name] = model;
+  }
+}
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
@@ -40,4 +53,4 @@ Object.keys(db).forEach(modelName => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-module.exports = db;
+export default db;
